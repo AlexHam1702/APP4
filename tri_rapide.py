e5ea5a23a806e84921f9b744c273efc32ffb0898
matthieuyme <matthieuyme@gmail.com>
2025-05-18 21:08:40 +0200

    Create tri_rapide.py

---
+++ tri_rapide.py
@@ -0,0 +1,8 @@
+# Tri rapide
+def tri_rapide(tableau):
+    if len(tableau) <= 1:
+        return tableau
+    pivot = tableau[0]['distance']
+    plus_petit = [x for x in tableau[1:] if x['distance'] <= pivot]
+    plus_grand = [x for x in tableau[1:] if x['distance'] > pivot]
+    return tri_rapide(plus_petit) + [tableau[0]] + tri_rapide(plus_grand)
