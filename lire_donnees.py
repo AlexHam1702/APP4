e698397be87177ba3dda9d54d3c729d653d776cf
matthieuyme <matthieuyme@gmail.com>
2025-05-18 20:58:16 +0200

    Update lire_donnees.py

--- lire_donnees.py
+++ lire_donnees.py
@@ -1,1 +1,14 @@
-
+# Fonction pour lire les données du fichier CSV
+def lire_donnees(fichier):
+    liste_villes = []
+    with open(fichier, 'r', encoding='utf-8') as fichier_csv:
+        lecteur = csv.DictReader(fichier_csv, delimiter=',')
+        for ligne in lecteur:
+            try:
+                nom = ligne['nom_sans_accent']
+                latitude = float(ligne['latitude_mairie'])
+                longitude = float(ligne['longitude_mairie'])
+                liste_villes.append({'nom': nom, 'latitude': latitude, 'longitude': longitude})
+            except:
+                continue  # Ignore les lignes mal formées
+    return liste_villes
